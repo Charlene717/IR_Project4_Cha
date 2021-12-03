e2b8478d9a1b58dca35ab593136324085ad1d106
CharleneMSI <q2330416@gmail.com>
2021-12-03 13:25:25 +0800

    Delete redundant code

--- FUN_Separate_Para2Sent.R
+++ FUN_Separate_Para2Sent.R
@@ -1,24 +1,11 @@
 ##### Separate the sentence #####
-
-xmlTestPath = "D:/Dropbox/##_GitHub/0-R/IR_Project4_Cha/Database_TestXML/test1.xml"
-Output_Sum <- XML_to_df(xmlTestPath)
-XML.df <- Output_Sum[["XML.df"]]
-
-XML.df.Abs <- data.frame(Abstract = XML.df$Abstract)
-XML.df.Abs1 <- XML.df.Abs[1,]
-# tmp <- str_split(XML.df.Abs1, " ")
-# tmp <- substr(XML.df.Abs1, " ")
-
-tmp <- strsplit(as.character(XML.df.Abs1), "(?<=\\.|\\?)\\s(?=[A-Z])", perl = TRUE) # https://stackoverflow.com/questions/35304900/split-paragraph-into-sentences-in-r
-
-
-# XML.df.Abs.Sent <- sapply(strsplit(as.character(XML.df.Abs1), "(?<=\\.|\\?)\\s(?=[A-Z])", perl = TRUE), length)
-
-
 SplitPara2Sent = function(XML.df.Abs1){
   
       # https://stackoverflow.com/questions/35304900/split-paragraph-into-sentences-in-r
       tmp <- strsplit(as.character(XML.df.Abs1), "(?<=\\.|\\?)\\s(?=[A-Z])", perl = TRUE) 
+      # tmp <- str_split(XML.df.Abs1, " ")
+      # tmp <- substr(XML.df.Abs1, " ")
+      
       Output_Sent = tmp[[1]] %>% as.data.frame() #%>% as.character() %>% enc2utf8()
       
       # Remove <U+00A0>
@@ -31,7 +18,16 @@ SplitPara2Sent = function(XML.df.Abs1){
 return(Output_Sent)
 }
 
-Test2 <- SplitPara2Sent(XML.df.Abs[2,])
+
+## For Test
+xmlTestPath = "D:/Dropbox/##_GitHub/0-R/IR_Project4_Cha/Database_TestXML/test1.xml"
+Output_Sum <- XML_to_df(xmlTestPath)
+XML.df <- Output_Sum[["XML.df"]]
+
+XML.df.Abs <- data.frame(Abstract = XML.df$Abstract)
+XML.df.Abs1 <- XML.df.Abs[1,]
+
+Test <- SplitPara2Sent(XML.df.Abs[2,])
 
 
 
