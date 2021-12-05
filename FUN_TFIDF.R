9c689e6c20c9154387fd3110439580b854ced4a2
CharleneMSI <q2330416@gmail.com>
2021-12-05 16:50:55 +0800

    Modify TFIDF

--- FUN_TFIDF.R
+++ FUN_TFIDF.R
@@ -1,5 +1,5 @@
 ## bind_tf_idf {tidytext}
-function (tbl, term, document, n) 
+TF_IDF = function (tbl, term, document, n , mode = "Basic")
 {
   term <- quo_name(enquo(term))
   document <- quo_name(enquo(document))
@@ -8,13 +8,36 @@ function (tbl, term, document, n)
   documents <- as.character(tbl[[document]])
   n <- tbl[[n_col]]
   doc_totals <- tapply(n, documents, sum)
-  idf <- log(length(doc_totals)/table(terms))
-  tbl$tf <- n/as.numeric(doc_totals[documents])
-  tbl$idf <- as.numeric(idf[terms])
+  
+  if(mode == "Basic"){
+      idf <- log(length(doc_totals)/table(terms))
+      tbl$tf <- n/as.numeric(doc_totals[documents])
+      tbl$idf <- as.numeric(idf[terms])
+      
+  }else if(mode == "A"){
+      idf <- log(length(doc_totals)/table(terms))
+      tbl$tf <- n/as.numeric(doc_totals[documents])
+      
+      tbl[tbl$tf > 0,  ]$tf <- 1
+      if(nrow(tbl[tbl$tf <= 0, ])>0){
+      tbl[tbl$tf <= 0, ]$tf <- 0
+      }
+      
+      tbl$idf <- as.numeric(idf[terms])
+    
+  }else if(mode == "B"){
+      idf <- log(length(doc_totals)/table(terms))
+      tbl$tf <- n/as.numeric(doc_totals[documents])
+      maxtf <- max(tbl$tf)
+      tbl$tf <- 0.5 + 0.5*tbl$tf/maxtf
+      tbl$idf <- as.numeric(idf[terms])
+  }
   tbl$tf_idf <- tbl$tf * tbl$idf
   if (any(tbl$idf < 0, na.rm = TRUE)) {
     rlang::warn(paste("A value for tf_idf is negative:\n", 
                       "Input should have exactly one row per document-term combination."))
   }
   tbl
+  
+  return(tbl)
 }
